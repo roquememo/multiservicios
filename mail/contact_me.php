cb928893bf192a39caab9df528faca516334de4a
Guillermo Roque <roquememo@hotmail.com>
2017-08-23 16:41:38 -0600

    Dandole funcinalidad al formulario de contacto

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,5 +1,8 @@
 <?php
-// check if fields passed are empty
+date_default_timezone_set('Etc/UTC');
+
+require '../PHPMailer/PHPMailerAutoload.php';
+
 if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
    empty($_POST['message'])	||
@@ -8,17 +11,88 @@ if(empty($_POST['name'])  		||
 	echo "No arguments Provided!";
 	return false;
    }
+
+   	$name = $_POST['name'];
+	$email_address = $_POST['email'];
+	$message = $_POST['message'];
+
+   	$email_subject1 = "Soporte de contacto: ";
+			
+	$email_body1 = "<p>Ha sido contactado por: <b>".$name."</b>, Su mensaje es el siguiente.</p>
+							<p>-----------------------------------</p>
+							<p>".preg_replace("/[\r\n]/i", "<br />", $message)."</p>
+							<p>-----------------------------------</p>
+							<p>
+								Ponte en contacto, Email: <a href=\"mailto:".$email_address."\">".$email_address."</a>
+							</p>";
+
+	$email_subject2 = "Confirmacion de envio: ";
+			
+	$email_body2 = "<p>Hola <b>".$name."</b> Gracias por escribirnos, Su mensaje:</p>
+							<p>-----------------------------------</p>
+							<p>".preg_replace("/[\r\n]/i", "<br />", $message)."</p>
+							<p>-----------------------------------</p>
+							<p>
+							Se ha enviado con éxito, Dentro de unos instantes uno de nuestros colaboradores se pondrá en contacto con usted.
+							</p>";
 	
-$name = $_POST['name'];
-$email_address = $_POST['email'];
-$message = $_POST['message'];
-	
-// create email body and send it	
-$to = 'YOUR-EMAIL@YOUR-WEBSITE.com'; // ****PUT YOUR EMAIL ADDRESS HERE****
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
-$headers = "From: CONTACT-FORM@YOUR-WEBSITE.com\n"; // ****ENTER WHO YOU WANT THE MESSAGE TO BE FROM HERE****
-$headers .= "Reply-To: $email_address";	
-mail($to,$email_subject,$email_body,$headers);
-return true;			
+
+$mail=new PHPMailer();
+$mail2=new PHPMailer();
+
+$mail->isSMTP();
+$mail2->isSMTP();
+
+$mail->SMTPDebug = 0;
+$mail2->SMTPDebug = 0;
+
+$mail->Debugoutput = 'html';
+$mail2->Debugoutput = 'html';
+
+$mail->Host = 'smtp.gmail.com';
+$mail2->Host = 'smtp.gmail.com';
+
+$mail->Port = 587;
+$mail2->Port = 587;
+
+$mail->SMTPSecure = 'tls';
+$mail2->SMTPSecure = 'tls';
+
+$mail->SMTPAuth = true;
+$mail2->SMTPAuth = true;
+
+$mail->Username = "multiservicioscasko@gmail.com";
+$mail2->Username = "multiservicioscasko@gmail.com";
+
+$mail->Password = "Admin.12345";
+$mail2->Password = "Admin.12345";
+
+$mail->setFrom('multiservicioscasko@gmail.com', 'Multiservicios Casko');
+$mail2->setFrom('multiservicioscasko@gmail.com', 'Multiservicios Casko');
+
+$mail->addAddress('roquememo@hotmail.com', 'Administrador');
+$mail2->addAddress('roquememo11@gmail.com', $name);
+
+$mail->Subject = $email_subject1;
+$mail->MsgHTML($email_body1);
+
+$mail2->Subject = $email_subject2;
+$mail2->MsgHTML($email_body2);
+
+if (!$mail->send()) {
+    echo "Mailer Error: " . $mail->ErrorInfo;
+} else {
+    echo "Message enviado!";
+}
+
+if (!$mail2->send()) {
+    echo "Mailer Error: " . $mail2->ErrorInfo;
+} else {
+    echo "Message enviado!";
+}
+
+
+
+
+		
 ?>
